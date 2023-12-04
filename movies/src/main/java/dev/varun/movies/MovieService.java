bc7b800d5cb92c18817287fbedb50ac0d43de14c
Varun Malleshwaram <84590524+mallisvarun@users.noreply.github.com>
2023-12-04 16:46:34 +0530

    second commit

--- movies/src/main/java/dev/varun/movies/MovieService.java
+++ movies/src/main/java/dev/varun/movies/MovieService.java
@@ -1,15 +1,21 @@
 package dev.varun.movies;
 //this class is for database access methods
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
-    @Autowired // instead of writing a constrcutor and instantiating the class ,we can just use this annotation so that it instantiates automatically for us
+    @Autowired // instead of writing a constructor and instantiating the class ,we can just use this annotation so that it instantiates automatically for us
     private MovieRepository movieRepository;
     public List<Movie> allMovies(){
+        return movieRepository.findAll();
 
+    }
+    public Optional<Movie> singleMovie(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
